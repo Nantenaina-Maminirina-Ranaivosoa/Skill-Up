b2835461ce1e35c72965d2d794044a99044f63f6
NanteRanaivosoa <rnantenainamaminirina@gmail.com>
2025-05-22 08:20:05 +0300

    Creation basic pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur SkillUp</h1>
+      <p>Votre plateforme d'apprentissage personnalisée</p>
+    </div>
+  );
+}
+
+export default Home;
